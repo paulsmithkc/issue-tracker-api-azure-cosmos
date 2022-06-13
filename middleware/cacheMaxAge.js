0e8b345d8d249b293294097e5a51c6ada6c035d4
Paul Smith <prsmith@ranken.edu>
2022-06-13 15:49:55 -0500

    Adding vary header for caching, based on authorization header

--- middleware/cacheMaxAge.js
+++ middleware/cacheMaxAge.js
@@ -2,6 +2,7 @@ export function cacheMaxAge(maxAgeInSeconds) {
   return (req, res, next) => {
     if (req.method === 'GET') {
       res.set('Cache-control', `max-age=${maxAgeInSeconds}`);
+      res.set('Vary', 'Authorization');
     } else {
       res.set('Cache-control', `no-store`);
     }
